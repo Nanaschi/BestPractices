4b537c0ca8f1715ccdd59eb01615ca43c0649344
Efremov Danil <naturforscher@outlook.com>
2022-03-07 11:28:48 +0300

    Added a very initial Input system

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -3,11 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
 public class PlayerController : MonoBehaviour
 {
     private PlayerControls _playerControls;
     private Rigidbody2D _rigidbody2D;
-    [SerializeField] private Vector2 _jumpForce;
+    private Vector2 _moveDirection;
+    [SerializeField] [Range(0, 500)] private int _playerSpeed;
+    [SerializeField] [Range(0, 500)] private int _jumpForce;
+    
 
     private void OnEnable()
     {
@@ -21,13 +25,30 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
+        UnityComponentsInitialization();
+        InputSystemInitialization();
+    }
+
+    private void InputSystemInitialization()
+    {
         _playerControls = new PlayerControls();
-        _rigidbody2D = GetComponent<Rigidbody2D>();
         _playerControls.Player.Jump.performed += context => Jump();
+        _playerControls.Player.Move.performed += context => _moveDirection = context.ReadValue<Vector2>();
+    }
+    
+
+    private void UnityComponentsInitialization()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     private void Jump()
     {
-        _rigidbody2D.AddForce(_jumpForce);
+        _rigidbody2D.AddForce(Vector2.up * _jumpForce);
+    }
+
+    private void Update()
+    {
+        _rigidbody2D.AddForce(_moveDirection * _playerSpeed);
     }
 }
