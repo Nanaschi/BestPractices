bff0fcc62a1e1c48812dc142a0ab4891c0ede5ec
Efremov Danil <naturforscher@outlook.com>
2022-03-19 19:50:55 +0300

    Added DOTween.Sequence().Append()

--- Assets/_Scripts/DOtween/SpheresTween.cs
+++ Assets/_Scripts/DOtween/SpheresTween.cs
@@ -14,6 +14,14 @@ public class SpheresTween : MonoBehaviour
     
     private void Start()
     {
+        SequenceTween();
+        //SpheresTweenLogic();
+    }
+
+    private void SpheresTweenLogic()
+    {
+        
+
         for (int i = 0; i < _sphereColliders.Length; i++)
         {
             _sphereColliders[0].transform
@@ -26,8 +34,7 @@ public class SpheresTween : MonoBehaviour
                         {
                             _sphereColliders[2].transform
                                 .DOMoveX(_xAxisMovement,
-                                    Random.Range(_randomMovementBorders.x, _randomMovementBorders.y)).
-                                OnComplete(() =>
+                                    Random.Range(_randomMovementBorders.x, _randomMovementBorders.y)).OnComplete(() =>
                                 {
                                     foreach (var sphereCollider in _sphereColliders)
                                     {
@@ -37,6 +44,28 @@ public class SpheresTween : MonoBehaviour
                         });
                 });
         }
+    }
+
+    private void SequenceTween()
+    {
+        var sequence = DOTween.Sequence();
+
+        foreach (var sphereCollider in _sphereColliders)
+        {
+            sequence.Append(sphereCollider.transform
+                .DOMoveX(_xAxisMovement, Random.Range(_randomMovementBorders.x, _randomMovementBorders.y)));
+            
+        }
         
+        sequence.OnComplete(() =>
+        {
+            foreach (var sphereCollider in _sphereColliders)
+            {
+                sphereCollider.transform.DOScale(Vector3.zero, .5f).SetEase(Ease.InBounce);
+            }
+            
+        });
+
+  
     }
 }
